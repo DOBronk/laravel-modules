cbc94aade2523669445c60ca09ac11840092b843
Dennis Bronk <dbronk2@gmail.com>
2024-07-27 18:32:57 +0200

    Guaranteed mess, first stupid commit

--- studentmanager/routes/web.php
+++ studentmanager/routes/web.php
@@ -15,8 +15,7 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     // This route only for at minimum a student
-    Route::get('/persons', [ControllerPerson::class, 'index'])->middleware(CheckRole::class . ':ROLE_MENTOR')->name('persons.list2');
-    Route::get('/persons', [ControllerPerson::class, 'index'])->middleware(CheckRole::class . ':ROLE_STUDENT');
+    Route::get('/persons', [ControllerPerson::class, 'index'])->middleware([CheckRole::class . ':ROLE_MENTOR',CheckRole::class . ':ROLE_STUDENT']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
