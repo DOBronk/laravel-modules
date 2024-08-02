98094a7458dcc23ecb8b770abd1f1b5c570a228e
Dennis Bronk <dbronk2@gmail.com>
2024-08-02 23:10:55 +0200

    Changed the welcome page for non authorized users and added login/register links

--- studentmanager/resources/views/welcome.blade.php
+++ studentmanager/resources/views/welcome.blade.php
@@ -1,5 +1,16 @@
-@extends('template')
-
-@section('content')
-    <h1>Student Manager</h1>
-@endsection
+    @if (auth()->check())
+        <script>
+            window.location.replace("{{ route('dashboard') }}")
+        </script>
+    @endif
+    @extends('template')
+    @section('content')
+        <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Student Manager</h1>
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">Welkom bij het student manager project van dennis. U ben't
+            nog niet ingelogged, om gebruik te kunnen maken van
+            deze
+            applicatie
+            dient u in te loggen.</p><br>
+        <x-nav-link href="{{ route('login') }}">Inloggen</x-nav-link> <x-nav-link
+            href="{{ route('register') }}">Registeren</x-nav-link>
+    @endsection
