24f0d9804c619308dd692c70cd6e0dc18343d147
Dennis Bronk <dbronk2@gmail.com>
2024-08-07 22:55:00 +0200

    Zaadje om de database te planten.

--- studentmanager/database/seeders/DatabaseSeeder.php
+++ studentmanager/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Schoolclass;
+use App\Models\Roles;
+
+use Illuminate\Support\Facades\Hash;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +19,63 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $sid = Roles::create([
+            'name' => 'ROLE_STUDENT',
+            'description' => 'Student',
         ]);
+        $mid = Roles::create([
+            'name' => 'ROLE_MENTOR',
+            'description' => 'Mentor',
+        ]);
+        $pid = Roles::create([
+            'name' => 'ROLE_PARENT',
+            'description' => 'Parent',
+        ]);
+        $adid = Roles::create([
+            'name' => 'ROLE_ADMIN',
+            'description' => 'Administrator',
+        ]);
+
+        $admin = User::create([
+            'name' => 'Dennis Administrator',
+            'email' => 'test@person.nl',
+            'dob' => '1989/12/1',
+            'phone' => '0687654321',
+            'password' => Hash::make('wachtwoord')
+        ]);
+
+        $student = User::create([
+            'name' => 'Test Student',
+            'email' => 'student@person.nl',
+            'dob' => '1989/12/01',
+            'phone' => '06876543231',
+            'password' => Hash::make('wachtwoord')
+        ]);
+
+        $mentor = User::create([
+            'name' => 'Test Mentor',
+            'email' => 'mentor@person.nl',
+            'phone' => '06876543231',
+            'password' => Hash::make('wachtwoord')
+        ]);
+
+        $parent = User::create([
+            'name' => 'Test Ouder',
+            'email' => 'ouder@person.nl',
+            'dob' => '1989/12/01',
+            'phone' => '06876543231',
+            'password' => Hash::make('wachtwoord')
+        ]);
+
+        $class = Schoolclass::create([
+            'name' => '1A',
+            'year' => '1',
+            'mentor_id' => $mentor->id,
+        ]);
+
+        $admin->roles()->sync($adid);
+        $student->roles()->sync($sid);
+        $mentor->roles()->sync($mid);
+        $parent->roles()->sync($pid);
     }
 }
