dd9ec3f721b46c2ea03ccf04f4aca7d79599865d
Dennis Bronk <dbronk2@gmail.com>
2024-10-11 15:37:25 +0200

    Standaard channel verwijderd

--- studentmanager/routes/channels.php
+++ studentmanager/routes/channels.php
@@ -6,7 +6,3 @@ use App\Models\User;
 Broadcast::channel('messages.{userId}', function (User $user, int $userId) {
     return $user->id === $userId;
 });
-
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
