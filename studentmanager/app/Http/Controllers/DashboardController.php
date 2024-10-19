8ed786232548f3237c70674a4d0e25122e616d9e
Dennis Bronk <dbronk2@gmail.com>
2024-10-19 19:27:09 +0200

    phpstorm test

--- studentmanager/app/Http/Controllers/DashboardController.php
+++ studentmanager/app/Http/Controllers/DashboardController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Role;
+use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
     const ROLES = ['ROLE_STUDENT', 'ROLE_MENTOR', 'ROLE_PARENT', 'ROLE_ADMIN'];
-    public function show(Request $request)
+    public function show(Request $request): View
     {
         $displayRoles = $request->user()->roles->filter(function (Role $role) {
             return in_array($role->name, DashboardController::ROLES);
