de58d4734210a020c9db741d479948d3d04b58e7
Dennis Bronk <dbronk2@gmail.com>
2025-01-30 18:36:21 +0100

    Whoops

--- modulairemonolith/Modules/CodeAnalyzer/resources/views/createjob1.blade.php
+++ modulairemonolith/Modules/CodeAnalyzer/resources/views/createjob1.blade.php
@@ -12,20 +12,20 @@
             <tr>
                 <th>Eigenaar</th>
                 <td>
-                    <input type="text" name="owner" :value="old('owner')" />
+                    <input type="text" name="owner" value="{{ old('owner') }}" />
                     <x-input-error :messages="$errors->get('owner')" class="mt-2" />
                 </td>
             </tr>
             <tr>
                 <th>Repository</th>
                 <td>
-                    <input type="text" name="repository" :value="old('repository')"/>
+                    <input type="text" name="repository" value="{{ old('repository') }}"/>
                     <x-input-error :messages="$errors->get('repository')" class="mt-2" />
                 </td>
             </tr>
             <tr>
                 <th>Branch</th>
-                <td><input type="text" name="branch" :value="old('branch')"/> (Optioneel veld, wanneer niet ingevuld wordt de standaard hoofdbranch gekozen)</td>
+                <td><input type="text" name="branch" value="{{ old('branch') }}"/> (Optioneel veld, wanneer niet ingevuld wordt de standaard hoofdbranch gekozen)</td>
             </tr>
             <tr>
                 <td colspan="2"><button type="submit">Volgende stap</button></td>
