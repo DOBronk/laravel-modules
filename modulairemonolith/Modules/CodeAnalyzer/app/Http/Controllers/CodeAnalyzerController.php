a03d662b2bf848abcf3360ca194869ffd9ae34a0
Dennis Bronk <dbronk2@gmail.com>
2025-01-30 17:09:47 +0100

    Added transaction for creating job, deleted obsolete code and included testfunction for messagebroker

--- modulairemonolith/Modules/CodeAnalyzer/app/Http/Controllers/CodeAnalyzerController.php
+++ modulairemonolith/Modules/CodeAnalyzer/app/Http/Controllers/CodeAnalyzerController.php
@@ -3,22 +3,14 @@
 namespace Modules\CodeAnalyzer\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Exception;
 use Illuminate\Http\Request;
 use Modules\CodeAnalyzer\Services\OllamaService;
-
 use Modules\CodeAnalyzer\Jobs\OllamaPromptJob;
-use Illuminate\Support\Facades\Storage;
-
-use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Modules\CodeAnalyzer\Models\Jobs;
-
 use Modules\CodeAnalyzer\Services\GithubService;
-
-//use App\Services\GitHubService;
-
-use Modules\CodeAnalyzer\Services\RabbitMqService
+use Modules\CodeAnalyzer\Services\MessageBroker
 ;
 class CodeAnalyzerController extends Controller
 {
@@ -26,24 +18,25 @@ class CodeAnalyzerController extends Controller
     {
         return view('codeanalyzer::createjob1');
     }
-
-    public function createStepTwo(Request $request, GitHubService $git, OllamaService $lama)
+    public function createStepTwo(Request $request, GitHubService $git)
     {
         $data = $request->validate([
             'owner' => 'required|string',
             'repository' => 'required|string',
+            'branch' => 'nullable|string',
         ]);
 
         $repo = $data['repository'];
         $owner = $data['owner'];
-        // TODO: Proper error handling in both getPhpFilesFromTree and here
-        $items = $git->getPhpFilesFromTree($owner, $repo);
+        $branch = $data['branch'] ?? 'main';
 
-        // dd($this->buildTree($items));
+        // TODO: Proper error handling in both getPhpFilesFromTree and here
+        $items = $git->getPhpFilesFromTree($owner, $repo, $branch);
 
         return view('codeanalyzer::createjob2', [
-            'owner' => $request['owner'],
-            'repository' => $request['repository'],
+            'owner' => $owner,
+            'repository' => $repo,
+            'branch' => $branch,
             'items' => $this->buildTree($items),
         ]);
     }
@@ -53,38 +46,15 @@ class CodeAnalyzerController extends Controller
         $data = $request->validate([
             'owner' => 'required|string',
             'repository' => 'required|string',
+            'branch' => 'nullable|string'
         ]);
-
-        $repo = $data['repository'];
-        $owner = $data['owner'];
 
         return redirect()->route('codeanalyzer.create.step.two', [
-            'repository' => $repo,
-            'owner' => $owner
+            'repository' => $data['repository'],
+            'owner' => $data['owner'],
+            'branch' => $data['branch']
         ]);
     }
-
-    private function buildTree($files)
-    {
-        $tree = [];
-
-        foreach ($files as $file) {
-            $parts = explode('/', $file['path']);
-            $current = &$tree;
-
-            foreach ($parts as $part) {
-                if (!isset($current[$part])) {
-                    $current[$part] = [];
-                }
-                $current = &$current[$part];
-            }
-            $current['?'] = $file['sha'];
-            $current['*'] = $file['path'];
-        }
-
-        return $tree;
-    }
-
     public function postCreateStepTwo(Request $request)
     {
         $data = $request->validate([
@@ -93,49 +63,44 @@ class CodeAnalyzerController extends Controller
             'selectedItems.*' => 'required',
         ]);
 
-        $repo = $data['repository'];
-        $owner = $data['owner'];
+        DB::transaction(function () use ($request, $data) {
+            $repo = $data['repository'];
+            $owner = $data['owner'];
 
-        //    dd($request->selectedItems);
-
-        $job = Jobs::create([
-            'user_id' => $request->user()->id,
-            'owner' => $owner,
-            'repo' => $repo,
-            'active' => 1
-        ]);
-
-        foreach ($request->selectedItems as $item) {
-            $values = explode("|", $item);
-            $job->items()->create([
-                'path' => $values[1],
-                'blob_sha' => $values[0],
-                'status' => 0,
-                'results' => "0"
+            $job = Jobs::create([
+                'user_id' => $request->user()->id,
+                'owner' => $owner,
+                'repo' => $repo,
+                'active' => 1
             ]);
-        }
 
-        dispatch(new OllamaPromptJob($job));
+            foreach ($request->selectedItems as $item) {
+                $values = explode("|", $item);
+                $job->items()->create([
+                    'path' => $values[1],
+                    'blob_sha' => $values[0],
+                    'status' => 0,
+                    'results' => "0"
+                ]);
+            }
+
+            dispatch(new OllamaPromptJob($job));
+        });
 
         return redirect()->route("codeanalyzer.index");
-        // return view('codeanalyzer::createjob2', ['repository' => $repo, 'owner' => $owner, 'items' => $items]);
     }
 
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index(Request $request, MessageBroker $broker)
     {
-        // $jobs = Jobs::where('active', '=', 1, 'and', 'user_id', '=', $request->user()->id)->get();
+        // TODO: Remove this line of code
+        $broker->addJob("testbericht");
+
         $jobs = Jobs::where('user_id', '=', $request->user()->id)->get();
 
-        /*    if ($jobs->count() > 0) {
-                $job = $jobs->first();
-                $items = $job->items()->get();
-                return view('codeanalyzer::activejob', ['items' => $items]);
-            } */
-
-        return view('codeanalyzer::jobdetails', ['items' => $jobs]);
+        return view('codeanalyzer::index', ['items' => $jobs]);
     }
     /**
      * Show the form for creating a new resource.
@@ -148,35 +113,8 @@ class CodeAnalyzerController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request, GitHubService $git)
+    public function store(Request $request)
     {
-        // $git = app(GithubService::class);
-        $owner = "DOBronk";
-        $repo = "laravel";
-
-        $job = Jobs::create([
-            'user_id' => $request->user()->id,
-            'owner' => $owner,
-            'repo' => $repo,
-            'active' => 1
-        ]);
-
-        $kill = $git->getPhpFilesFromTree("DOBronk", "laravel"); // "1087afb04cfa349f6bbc397b0d14691176027315");
-
-        $i = 0;
-        foreach ($kill as $item) {
-            $job->items()->create([
-                'path' => $item['path'],
-                'blob_sha' => $item['sha'],
-                'status' => 0
-            ]);
-            $i++;
-            if ($i == 10) {
-                break;
-            }
-        }
-
-        dispatch(new OllamaPromptJob($job));
     }
 
     /**
@@ -209,5 +147,25 @@ class CodeAnalyzerController extends Controller
     public function destroy($id)
     {
         //
+    }
+    private function buildTree($files)
+    {
+        $tree = [];
+
+        foreach ($files as $file) {
+            $parts = explode('/', $file['path']);
+            $current = &$tree;
+
+            foreach ($parts as $part) {
+                if (!isset($current[$part])) {
+                    $current[$part] = [];
+                }
+                $current = &$current[$part];
+            }
+            $current['?'] = $file['sha'];
+            $current['*'] = $file['path'];
+        }
+
+        return $tree;
     }
 }
